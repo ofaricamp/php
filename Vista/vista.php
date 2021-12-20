81c556acf118087acf5fb6e4f81a8074917b5cb8
Oscar fariña <61348449+ofaricamp@users.noreply.github.com>
2021-12-20 12:24:11 +0100

    Add files via upload

---
+++ Vista/vista.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de articulos</title>
+</head>
+<body>
+     <!-- Poner todo el codigo HTML correspondiente, la idea basica es el hacer
+    una pagina de compra de juegos de mesa sencilla (si es posible el meter la mitica
+    opcion de filtros de busqueda) aunque al ser solo un pequeño proyecto de practica
+    no debe quedar una obra maestra pero prefreriblemente dejarlo bien para poder 
+    usarlo de ejemplo para el proyecto final-->
+</body>
+</html>
+
+<?php  ?>
